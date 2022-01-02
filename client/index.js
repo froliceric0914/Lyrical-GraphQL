50efa06e5afe974b67ee8923f1555c8b4757a231
froliceric0914 <wzhao.eric@gmail.com>
2022-01-02 14:29:45 -0500

    add apolloProvider, apolloClient and gql to render the songlist

--- client/index.js
+++ client/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//apollo communicate with BE
+import ApolloClient from 'apollo-client'; 
+//apollo customized for react, which provides the data for client side
+import { ApolloProvider } from 'react-apollo'; 
+
+import SongList from './src/SongList';
+
+const client = new ApolloClient({}); //initiate empty store
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <SongList />
+    </ApolloProvider>)
 };
 
 ReactDOM.render(
---
+++ client/src/SongList.js
@@ -0,0 +1,43 @@
+import React, { Component} from 'react';
+import gql from 'graphql-tag' //data query helper
+import { graphql } from 'react-apollo' //the helper to bond the query to client side
+
+
+
+
+//data required is the song titles
+class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      )
+    })
+  }
+
+  render() {
+    //gql will first send the request and then component will be re-rendered when the data comes back
+    if (this.props.data.loading) { return <div>Loading...</div>}
+    console.log(this.props)
+    return (
+      <ul className="collection">
+        {this.renderSongs()}
+      </ul>
+    ); 
+  }   
+}
+
+//use template string and gql helper to form the query data to be executed
+const query = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`;
+
+//graphql as a glue to bond the data defined/formed from be to fe
+export default graphql(query)(SongList);
