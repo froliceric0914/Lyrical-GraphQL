451ed872e36e5810159c70318925611c0d3bc1d8
froliceric0914 <wzhao.eric@gmail.com>
2022-01-02 12:54:46 -0500

    connect to mongoDB cloud

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://froliceric0914:VuNzu%40jcrZ5Znkw@cluster0.97kfd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
