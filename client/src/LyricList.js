cb0b53476b6bd75f49722f77e9e86dcb440bbde6
froliceric0914 <wzhao.eric@gmail.com>
2022-01-09 23:35:58 -0500

    feat: add and fetch Likes to lyrics

--- client/src/LyricList.js
+++ client/src/LyricList.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-// import { graphql } from 'react-apollo';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 class LyricList extends Component {
+  onLike(songId) {
+    this.props.mutate({ variables: { songId } });
+  }
+
   renderLyrics() {
-    return this.props.lyrics.map(({ content, id }) => (
+    return this.props.lyrics.map(({ id, content, likes }) => (
       <li className="collection-item" key={id}>
         {content}
+        <div className="vote-box>">
+          <i className="material-icons" onClick={() => this.onLike(id)}>
+            thumb_up
+          </i>
+          {likes}
+        </div>
       </li>
     ));
   }
 
   render() {
-    return <ul className="colletion">{this.renderLyrics()}</ul>;
+    return <ul className="collection">{this.renderLyrics()}</ul>;
   }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyrics($songId: ID!) {
+    likeLyric(id: $songId) {
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
